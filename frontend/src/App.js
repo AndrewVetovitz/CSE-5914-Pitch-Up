1a810bc3dcefb860aa2d0c369bd80cc0958b7d86
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2018-08-30 17:04:10 -0400

    added http request to get data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    constructor(){
+        super();
+        this.state = {
+            phrase: ''
+        };
+    }
+
+    UNSAFE_componentWillMount(){
+        fetch('http://localhost:5000/').then(response => {
+            return response.text();
+        }).then(data => {
+            this.setState({phrase: data});
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    {this.state.phrase}
+                </p>
+            </div>
+        );
   }
 }
 
--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('*', (req, res) => {
     res.send('hello');
 });
 
 app.listen(port, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('Server started on port %s', port);
 });
