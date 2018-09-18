ca44dfbdca24fde0ef83d5b3b94b7699ca51184c
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2018-09-18 12:29:09 -0400

    added email to database relational table

--- app/server/models/user.py
+++ app/server/models/user.py
@@ -8,8 +8,9 @@ class User(Base):
 
      id = Column(Integer, primary_key=True)
      name = Column(String)
+     email = Column(String)
      password = Column(String)
 
      def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-            self.name, self.fullname, self.password)
+        return "<User(name='%s', fullname='%s', email='%s', password='%s')>" % (
+            self.name, self.fullname, self.email, self.password)
