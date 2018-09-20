59cabc44f1d5af3f706d0fee85cb7863a4df7ed6
Chris Ellis <christopherje84@hotmail.com>
2018-09-20 17:11:26 -0400

    added flask sqlalchemy and db_create

--- app/server/app.py
+++ app/server/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from controllers.user import user_blueprint
 from controllers.report import report_blueprint
-import sys
+
+
+SQLITE_DB_LOCATION = 'sqlite:///db/test.db'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLITE_DB_LOCATION
+db = SQLAlchemy(app)
 
 app.register_blueprint(user_blueprint)
 
