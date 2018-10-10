8956351e089c622917a001f9322315c1eb4fb68e
Blaise Marchetti <marchetti.14@osu.edu>
2018-10-10 13:24:21 -0400

    Refactoring front end

---
+++ app/frontend/src/components/DashCards/CursesCard.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardIcon from "components/Card/CardIcon.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import Icon from "@material-ui/core/Icon";
+
+export default class StopWordsCard extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card>
+        <CardHeader color="danger" stats icon>
+          <CardIcon color="danger">
+            <Icon>warning</Icon>
+          </CardIcon>
+          <p className={classes.cardCategory}>Number of Expletives</p>
+          <h3 className={classes.cardTitle}>
+            {this.props.explitives} <small>curses</small>
+          </h3>
+        </CardHeader>
+        <CardFooter stats>
+          <div className={classes.stats}>
+          </div>
+        </CardFooter>
+      </Card>
+)
+  }
+}
