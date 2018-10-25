96f446d85e8051ed9629f55c447a493c0943526f
Chris Ellis <christopherje84@hotmail.com>
2018-10-25 11:11:13 -0400

    dummy data for demo in dev.db

--- app/server/db_dummy.py
+++ app/server/db_dummy.py
@@ -23,26 +23,56 @@ def add_dummy_data(environment):
         flask_app.app_context().push()
 
 
-        # Create some dummy data
+        # Create some useful dummy data for demos
         user = User(name = "Chris Ellis", email = "chris@bingoogle.com", password="123456")
         db.session.add(user)
         db.session.commit()
         print("Added:", user)
 
+        # Audio engineering
         pitch = Pitch(name = "Amazing new speaker technology", user_id = user.id)
         db.session.add(pitch)
         db.session.commit()
         print("Added:", pitch)
 
-        pitch_try = PitchTry(pitch_id=pitch.id, transcription="Hey there you're such shit", duration=12)
+        pitch_try = PitchTry(pitch_id=pitch.id, transcription="Hey there I am doing research for new audio technologies are you interested in hearing more", duration=8)
         db.session.add(pitch_try)
         db.session.commit()
         print("Added:", pitch_try)
 
-        pitch_try = PitchTry(pitch_id=pitch.id, transcription="Hey i am interested in audio engineering how about you there are some cool things we can do with it", duration=20)
+        pitch_try = PitchTry(pitch_id=pitch.id, transcription="Hey i am interested in audio engineering how about you there are some cool things we can do with it", duration=12)
         db.session.add(pitch_try)
         db.session.commit()
         print("Added:", pitch_try)
+
+        pitch_try = PitchTry(pitch_id=pitch.id, transcription="My name is Chris and I work with audio technologies we have new research that is paving the way for some exciting breakthroughs that will push the boundaries of audio engineering as we know it today", duration=20)
+        db.session.add(pitch_try)
+        db.session.commit()
+        print("Added:", pitch_try)
+
+        # Pet sitting service
+        # pitch = Pitch(name = "Pet sitting service", user_id = user.id)
+        # db.session.add(pitch)
+        # db.session.commit()
+        # print("Added:", pitch)
+
+        # pitch_try = PitchTry(pitch_id=pitch.id, transcription="Hey I run a pet sitting service. If you have a dog or cat i can pet sit it", duration=8)
+        # db.session.add(pitch_try)
+        # db.session.commit()
+        # print("Added:", pitch_try)
+
+        # pitch_try = PitchTry(pitch_id=pitch.id, transcription="I am a pet sitter. Call me if you need anyone to watch your pets! My number is 123 456 7829", duration=10)
+        # db.session.add(pitch_try)
+        # db.session.commit()
+        # print("Added:", pitch_try)
+
+        # pitch_try = PitchTry(pitch_id=pitch.id, transcription="I am a highly-rated pet sitter. I love watching pets and treat them like family. You can count on me if you ever are in need of a pet-sitter. my email is joe@pet.com", duration=13)
+        # db.session.add(pitch_try)
+        # db.session.commit()
+        # print("Added:", pitch_try)
+
+
+        
 
         ### User: Dummy Data
         ##############################################
